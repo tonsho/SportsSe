e7f16cf80b05b0c9c0ff02a3c89cd28526496009
Kazutaka Tonsho <Kazutaka.Tonsho@gmail.com>
2015-03-18 00:41:16 +0900

    Implement selectDate

--- JavaScript1.js
+++ JavaScript1.js
@@ -3,14 +3,28 @@ var facilities = {
     "運動公園": ["700", "900", "1100", "1300", "1500", "1700"]
 };
 
-var reservationList = {
-    "法典公園（グラスポ）": {
-        "2015/2/26": {
-            "1030": "",
-            "1430": ""
-        }
+
+var reservationList =
+    [
+    {
+        facility: "法典公園（グラスポ）",
+        date: "2015/2/26",
+        time_slot: [
+                "1030", "1430"]
     }
-};
+    ];
+
+function getTargetFaclity() {
+    return "法典公園（グラスポ）";
+}
+
+function getTargetDate() {
+    return "2015/4/30";
+}
+
+function moveToNextTarget() {
+    // TODO
+}
 
 function fillFacilityTable() {
     var selectOption = $("<select></select>");
@@ -42,18 +56,19 @@ function load() {
         setTimeout(selectFacility, getSleepTime(), contents);
     } else if (0 < title.indexOf("施設空き状況１ヶ月表示画面")) {
         setTimeout(selectDate, getSleepTime(), contents);
+    } else if (0 < title.indexOf("施設空き状況画面時間貸し")) {
+        setTimeout(selectTimeSlot, getSleepTime(), contents);
     }
 }
 
 function disableDialog(contents) {
     var doc = contents[0];
-    var disableConfirm = function() {
+    var disableConfirm = function () {
         window.confirm = function (message) {
             console.log("confirm '" + message + "' true");
             return true;
         }
     }
-    console.log(disableConfirm.toString());
     doc.location.href = "javascript:(" + disableConfirm.toString() + "());";
 }
 
@@ -84,37 +99,70 @@ function selectTennis(contents) {
 }
 
 function selectFacility(contents) {
-    var facility = getNextFaclity();
+    var facility = getTargetFaclity();
     dispatchClick(contents, $("a:contains('" + facility + "')", contents));
 }
 
-function getNextFaclity() {
-    return "法典公園（グラスポ）";
-}
-
 function selectDate(contents) {
-    var date = new Date(getNextDate());
-    var targetMonth = (date.getDate() + 1);
-    var displayMonth = getDisplayMonth(contents);
-    console.log("target : " + targetMonth + ", displaying : " + displayMonth);
+    var date = new Date(getTargetDate());
+    var targetMonth = (date.getMonth() + 1);
+    var displayingMonth = getDisplayingMonth(contents);
+    console.log("target : " + targetMonth + ", displaying : " + displayingMonth);
 
-    if (targetMonth < displayMonth) {
-        movePreviousMonth();
+    if (targetMonth < displayingMonth) {
+        movePreviousMonth(contents);
         return;
-    } else if (targetMonth > displayMonth) {
-        moveNextMonth();
+    } else if (targetMonth > displayingMonth) {
+        moveNextMonth(contents);
         return;
     }
-
-    
+    var fmt = new DateFormat("yyyy, M, d");
+    var targetDateLink = $("a[href*='" + fmt.format(date) + "']");
+    if (targetDateLink.length) {
+        dispatchClick(contents, targetDateLink);
+    } else {
+        console.log("There is no space. " + fmt.format(date));
+        moveToNextTarget();
+    }
 }
 
-function getNextDate() {
-    return "2015/2/26";
+function selectTimeSlot(contents) {
+    var date = new Date(getTargetDate());
+    var targetMonth = (date.getMonth() + 1);
+    var displayingMonth = getDisplayingMonth(contents);
+    console.log("target : " + targetMonth + ", displaying : " + displayingMonth);
+
+    if (targetMonth < displayingMonth) {
+        movePreviousMonth(contents);
+        return;
+    } else if (targetMonth > displayingMonth) {
+        moveNextMonth(contents);
+        return;
+    }
+    var fmt = new DateFormat("yyyy, M, d");
+    var targetDateLink = $("a[href*='" + fmt.format(date) + "']");
+    if (targetDateLink.length) {
+        dispatchClick(contents, targetDateLink);
+    } else {
+        console.log("There is no space. " + fmt.format(date));
+        moveToNextTarget();
+    }
+}
+
+function getDisplayingMonth(contents) {
+    var yearMonthString = $("strong:contains('" + new Date().getFullYear() + "年')", contents).text();
+    console.log("yearMonthString : " + yearMonthString);
+    var fmt = new DateFormat("yyyy年M月");
+    var date = fmt.parse(yearMonthString);
+    return date.getMonth() + 1;
+}
+
+function movePreviousMonth(contents) {
+    dispatchClick(contents, $("img[alt='前の月']", contents).parent());
 }
 
 function moveNextMonth(contents) {
-
+    dispatchClick(contents, $("img[alt='次の月']", contents).parent());
 }
 
 function dispatchClick(contents, a) {
