2c387df9f9ded0af20b966d139160b551370f509
Kazutaka Tonsho <Kazutaka.Tonsho@gmail.com>
2016-04-24 23:36:14 +0900

    Run as node.js to enable http schema

---
+++ app.js
@@ -0,0 +1,24 @@
+﻿
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.static(__dirname));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
