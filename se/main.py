2543bad09e592660ee8befaaf57ca1167cc6a62f
Kazutaka Tonsho <Kazutaka.Tonsho@gmail.com>
2019-05-05 21:56:25 +0900

    Update selenium

--- se/main.py
+++ se/main.py
@@ -84,8 +84,9 @@ def start(rsv_info, rsv_list):
             log.info('Sleep until {} ({}[sec])'.format(datetime.fromtimestamp(reservation_start), reservation_start - now))
             time.sleep(reservation_start - now)
 
-    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
-    brw = webdriver.PhantomJS(desired_capabilities={'phantomjs.page.settings.userAgent':user_agent})
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    brw = webdriver.Chrome(options=options)
     brw.get('https://funayoyaku.city.funabashi.chiba.jp/web/')
 
     loop(brw, rsv_info, rsv_list)
